37738dc4a4d0f599f09801c28175c255b10d3317
Belle Xu <bellex22@protonmail.com>
2023-08-10 17:53:47 -0400

    first code review done

--- db.js
+++ db.js
@@ -4,12 +4,12 @@
 
 const { Client } = require("pg");
 
-// const DB_URI = process.env.NODE_ENV === "test"  // 1
-//     ? "postgresql:///biztime_test"
-//     : "postgresql:///biztime";
 const DB_URI = process.env.NODE_ENV === "test"  // 1
-    ? "postgresql://ElectricSweater:newpassword@server/biztime_test "
-    : "postgresql://ElectricSweater:newpassword@server/biztime ";
+    ? "postgresql:///biztime_test"
+    : "postgresql:///biztime";
+// const DB_URI = process.env.NODE_ENV === "test"  // 1
+//     ? "postgresql://ElectricSweater:newpassword@server/biztime_test "
+//     : "postgresql://ElectricSweater:newpassword@server/biztime ";
 
 let db = new Client({
   connectionString: DB_URI
--- routes/companies.js
+++ routes/companies.js
@@ -10,7 +10,6 @@ const {
 } = require("../expressError");
 
 const db = require("../db");
-const { request } = require("http");
 const router = new express.Router();
 
 
@@ -19,10 +18,10 @@ const router = new express.Router();
  */
 
 router.get("/", async function (req, res) {
-  const results = await db.query(`
-    SELECT code, name
-    FROM companies
-  `);
+  const results = await db.query(
+    `SELECT code, name
+      FROM companies`
+  );
 
   const companies = results.rows;
   return res.json({ companies });
@@ -37,13 +36,14 @@ router.get("/", async function (req, res) {
 router.get("/:code", async function (req, res) {
   const code = req.params.code;
 
-  const results = await db.query(`
-    SELECT code, name, description
-    FROM companies
-    WHERE code = $1
-  `, [code]);
+  const results = await db.query(
+    `SELECT code, name, description
+      FROM companies
+      WHERE code = $1`, [code]
+  );
 
   const company = results.rows[0];
+  if(!company) throw new NotFoundError(`No matching company: ${code}`);
   return res.json({ company });
 });
 
@@ -56,17 +56,19 @@ router.get("/:code", async function (req, res) {
 */
 
 router.post("/", async function (req, res) {
-  if (req.body === undefined) throw new BadRequestError()
+  if (req.body === undefined) throw new BadRequestError();
 
-  const results = await db.querty(`
-  INSERT INTO companies(code, name, description)
-  VALUES($1,$2,$3)
-  RETURN code, name, description
-  `, [req.body.code, req.body.name, req.body.description]);
+  const results = await db.query(
+    `INSERT INTO companies(code, name, description)
+      VALUES($1,$2,$3)
+      RETURNING code, name, description`,
+      [req.body.code, req.body.name, req.body.description]
+  );
+  console.log("results are", results);
 
   const company = results.rows[0];
   return res.status(201).json({ company });
-})
+});
 
 /** PUT /companies/[code]
  * Edit existing company.
@@ -77,6 +79,27 @@ Needs to be given JSON like: {name, description}
 
 Returns update company object: {company: {code, name, description}}
 */
+router.put("/:code", async function (req, res) {
+  if (req.body === undefined) throw new BadRequestError("Input required");
+  if ("code" in req.body) throw new BadRequestError("Not Allowed");
+
+  const code = req.params.code;
+  const results = await db.query(
+    `UPDATE companies
+    SET name=$1, description=$2
+    WHERE code = $3
+    RETURNING code,name, description
+  `, [req.body.name, req.body.description, code]
+  );
+
+  console.log("results are", results);
+
+  const company = results.rows[0];
+
+  if (!company) throw new NotFoundError(`No matching company: ${code}`);
+  return res.json({ company });
+});
+
 
 
 
@@ -88,5 +111,22 @@ Should return 404 if company cannot be found.
 
 Returns {status: "deleted"}
 */
+
+router.delete("/:code", async function (req, res) {
+
+  const code = req.params.code;
+  const results = await db.query(
+    `DELETE FROM companies WHERE code = $1
+    RETURNING code`, [code]
+  );
+  console.log("results are", results);
+
+  const company = results.rows[0];
+
+  if (!company) throw new NotFoundError(`No matching company: ${code}`);
+  return res.json({ status: "deleted" });
+});
+
+
 
 module.exports = router;
